0612756a6320885b52e89b44c4712d4726d9bd81
Mike Redrobe <mike@redrobe.net>
2015-04-12 23:44:00 +0100

    Create ledcheck.py
    Requires Pi "plus" models or more recent:
    A+ B+ Pi2
    
    Monitors power level, reports if low for longer than 4 seconds.

---
+++ ledcheck.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO , time
+
+redLED=35
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(redLED, GPIO.IN)
+
+powerlow=0
+while True:
+        if(GPIO.input(redLED)==0):
+                print "POWER dipped below 4.63v"
+                powerlow += 1
+        else:
+                powerlow =0
+        if (powerlow  > 3):
+                 print "Low power for " + str(powerloss) + " seconds"
+                 print "shutting down"
+        time.sleep(1)
